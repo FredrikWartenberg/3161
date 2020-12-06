1f2a0d917e60ff8642580ec9c76f06f458f0a6ee
Fredrik <wartenberg@wartenberg.se>
2020-12-06 20:41:51 +0100

    Auto-updated data 3161

--- push3161.bash
+++ push3161.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Copy
 cp /home/pi/remote/3161/data/*.samples /home/pi/gitRemote/3161/data/
 cp /home/pi/remote/3161/data/*.meta /home/pi/gitRemote/3161/data/
 
+# Push
 cd /home/pi/gitRemote/3161
 git add . -v
 git commit -m "Auto-updated data 3161"
